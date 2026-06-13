1dbe68651bd80747fbad594eab1f6136286e240a
Stefan Bogun <stefan.bogun@adevinta.com>
2026-06-13 16:41:32 +0200

    Revert "start nvimTree at nvim startup"
    This reverts commit f345cd37e8c0602cbea9b5840c3651b3b429dc60.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -66,11 +66,3 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.signcolumn = "no"
 	end,
 })
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function(data)
-    if data.file == "" then
-      require("nvim-tree.api").tree.open()
-    end
-  end,
-})
